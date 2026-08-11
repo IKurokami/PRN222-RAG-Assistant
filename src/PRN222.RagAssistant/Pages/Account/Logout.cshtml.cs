dcca52f1616dfe0266b96955829ef8f8e123c664
Đỗ Khắc Phú Vinh <0001.kurokami@gmail.com>
2026-08-11 15:12:30 +0700

    feat: add identity logout page model

---
+++ src/PRN222.RagAssistant/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PRN222.RagAssistant.Domain.Entities;
+
+namespace PRN222.RagAssistant.Pages.Account;
+
+[Authorize]
+public sealed class LogoutModel : PageModel
+{
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public LogoutModel(SignInManager<ApplicationUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToPage("/Index");
+    }
+}
