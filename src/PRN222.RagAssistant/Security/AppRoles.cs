62baa5f8ffc1e4d302d2954efebb4f0597b516be
Đỗ Khắc Phú Vinh <0001.kurokami@gmail.com>
2026-08-11 15:07:53 +0700

    feat: add application roles

---
+++ src/PRN222.RagAssistant/Security/AppRoles.cs
@@ -0,0 +1,10 @@
+namespace PRN222.RagAssistant.Security;
+
+public static class AppRoles
+{
+    public const string SubjectLeader = "SubjectLeader";
+
+    public const string Student = "Student";
+
+    public static readonly string[] All = [SubjectLeader, Student];
+}
