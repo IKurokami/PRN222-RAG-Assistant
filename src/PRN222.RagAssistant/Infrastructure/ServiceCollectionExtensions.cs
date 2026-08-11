bd86a70a4c9aaa8d4e3787a10404c0f81acfa79e
Đỗ Khắc Phú Vinh <0001.kurokami@gmail.com>
2026-08-11 15:09:09 +0700

    feat: integrate ef core identity and authorization

--- src/PRN222.RagAssistant/Infrastructure/ServiceCollectionExtensions.cs
+++ src/PRN222.RagAssistant/Infrastructure/ServiceCollectionExtensions.cs
@@ -1,4 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Npgsql;
+using Pgvector.EntityFrameworkCore;
+using PRN222.RagAssistant.Data;
+using PRN222.RagAssistant.Domain.Entities;
+using PRN222.RagAssistant.Security;
 
 namespace PRN222.RagAssistant.Infrastructure;
 
@@ -19,6 +25,32 @@ public static class ServiceCollectionExtensions
         var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
         dataSourceBuilder.UseVector();
         services.AddSingleton(dataSourceBuilder.Build());
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString, npgsql => npgsql.UseVector()));
+
+        services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequiredLength = 8;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.ConfigureApplicationCookie(options =>
+        {
+            options.LoginPath = "/Account/Login";
+            options.AccessDeniedPath = "/Account/AccessDenied";
+            options.SlidingExpiration = true;
+        });
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy(
+                AppPolicies.ManageDocuments,
+                policy => policy.RequireRole(AppRoles.SubjectLeader));
+        });
 
         var ollamaBaseUrl = configuration["Rag:Ollama:BaseUrl"];
 
